59d7043c3f5b08896435736ac930643d4f34490d
takawo <tkwshnsk@gmail.com>
2016-10-17 22:11:08 +0900

    add comment

--- 01_EmptyExample/src/main.cpp
+++ 01_EmptyExample/src/main.cpp
@@ -1,13 +1,14 @@
-#include "ofMain.h"
-#include "ofApp.h"
+#include "ofMain.h" // openFrameworksのライブラリの読み込み
+#include "ofApp.h" // ofApp.hの読み込み
+
+
 
 //========================================================================
+// 実行ファイル
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
+    // ウィンドウサイズを指定,OF_WINDOWをOF_FULLSCREENにすることで、フルスクリーンになる
+    ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+    // ofAppを実行
 	ofRunApp(new ofApp());
 
 }
--- 01_EmptyExample/src/ofApp.h
+++ 01_EmptyExample/src/ofApp.h
@@ -1,14 +1,18 @@
-#pragma once
+#pragma once // 1度だけ読み込み
 
-#include "ofMain.h"
+#include "ofMain.h" // openFrameworksのライブラリの読み込み
 
+//--------------------------------------------------------------
+// ofAppクラス: 開発するアプリケーションの中身
 class ofApp : public ofBaseApp{
-
+    
+    // public : 外部からもアクセスが可能なプロパティ（=変数）やメソッド(=関数)を記述する
 	public:
-		void setup();
-		void update();
-		void draw();
-
+		void setup(); // セットアップ
+		void update(); // アップデート
+		void draw(); //描画
+    
+        /* イベント関係はofApp.cppの各関数の行頭に記述 */
 		void keyPressed(int key);
 		void keyReleased(int key);
 		void mouseMoved(int x, int y );
@@ -20,5 +24,7 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
+    
+    // private : 外部からアクセスできないプロパティやメソッドを記述する
+    private:
 };
